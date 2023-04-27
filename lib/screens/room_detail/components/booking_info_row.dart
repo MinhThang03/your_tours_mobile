755be273e07a90210ba71cf85f8cbbd0c2202823
MinhThang03 <minhhoangthang75@gmail.com>
2023-04-27 23:05:56 +0700

    update: payment page UI

--- lib/screens/room_detail/components/booking_info_row.dart
+++ lib/screens/room_detail/components/booking_info_row.dart
@@ -9,38 +9,34 @@ class BookingInfoRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:
-          const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 0, top: 0),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w400, fontSize: 16),
-                    ),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                  ],
-                ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w400, fontSize: 16),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                ],
               ),
-              Expanded(
-                child: Text(
-                  content,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w600, fontSize: 16),
-                ),
+            ),
+            Expanded(
+              child: Text(
+                content,
+                style:
+                    const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
